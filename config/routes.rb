e63d536f31f9ea64ff43898221bca3fe5da29c75
szaxar <wiktorleczynski@gmail.com>
2018-05-18 15:05:44 +0200

    config routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'main#index'
+  match ':controller(/:action(/:id))', :via =>[:get , :post] 
   get 'register/index'
-  get 'main/index'
+  #get 'main/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
