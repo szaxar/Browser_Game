d9ed38202e1df2a2f83c1b829e8827786c36718f
Przemysław Moskała <przemyslaw.moskala@gmail.com>
2018-05-19 23:40:40 +0200

    Google authetnitaction

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, 'my Google client id', 'my Google client secret', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '764213219818-6mdil9ifct7aunbtrcdnaad8la0er3qb.apps.googleusercontent.com', '1yUMeVGsuOpstteglyZaF9lX', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
---
+++ docs/auth.md
@@ -0,0 +1,7 @@
+https://richonrails.com/articles/google-authentication-in-ruby-on-rails/
+
+1. bundle install
+2. In RubyMine Edit Configuration of Development: Browser_Game
+ by adding ```--ssl --ssl-key-file ./.ssl/localhost.key --ssl-cert-file ./.ssl/localhost.crt```
+ to server arguments
+ 
