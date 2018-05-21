afbcfa8111007ffebc6b609c661c5770de24cc87
Przemysław Moskała <przemyslaw.moskala@gmail.com>
2018-05-21 17:51:39 +0200

    User model update

--- db/migrate/20180519212215_create_users.rb
+++ db/migrate/20180519212215_create_users.rb
@@ -6,8 +6,22 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :oauth_token
       t.datetime :oauth_expires_at
-
+      t.string "nick" , :limit=>20 , :null => false
+      t.string "email" , :limit=>20 , :null => false
+      t.integer "hp" , :null => false, :default => 100
+      t.integer "lvl" , :null => false,  :default => 1
+      t.integer "experience" , :null => false,  :default => 0
+      t.integer "gold" , :null => false,  :default => 0
+      t.integer "attack" , :null => false, :default => 10
+      t.integer "defence" , :null => false, :default => 5
+      t.integer "strength" , :null => false,  :default => 10
+      t.integer "agility" , :null => false,  :default => 10
       t.timestamps
     end
+    add_index("users","nick")
+  end
+
+  def down
+    drop_table :users
   end
 end
