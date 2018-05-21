00567eeca8e31a74ac013b982196895a60a551a4
Przemysław Moskała <przemyslaw.moskala@gmail.com>
2018-05-21 20:14:00 +0200

    Add atributes

--- db/migrate/20180521174028_create_expeditions.rb
+++ db/migrate/20180521174028_create_expeditions.rb
@@ -4,15 +4,19 @@ class CreateExpeditions < ActiveRecord::Migration[5.2]
       t.string :name
       t.integer :duration
       t.integer :requiredLevel
-      t.integer :gainedExperience
-      t.integer :gainedGold
-      t.integer :enemyAttack
-      t.integer :enemyDefence
-      t.integer :enemyStrength
-      t.integer :enemyAgility
-      t.integer :enemyHp
-
+      t.integer :gainedExperience, :null => false
+      t.integer :gainedGold, :null => false
+      t.integer :enemyAttack, :null => false
+      t.integer :enemyDefence, :null => false
+      t.integer :enemyStrength, :null => false
+      t.integer :enemyAgility, :null => false
+      t.integer :enemyHp, :null => false, :default => 100
       t.timestamps
     end
+    add_index("expeditions","name")
+  end
+
+  def down
+    drop_table :expeditions
   end
 end
--- db/migrate/20180521180430_create_users.rb
+++ db/migrate/20180521180430_create_users.rb
@@ -6,18 +6,22 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :oauth_token
       t.datetime :oauth_expires_at
-      t.string :nick
-      t.string :email
-      t.integer :hp
-      t.integer :lvl
-      t.integer :experience
-      t.integer :gold
-      t.integer :attack
-      t.integer :defence
-      t.integer :strength
-      t.integer :agility
-
+      t.string :nick , :limit=>20 , :null => false, :unique => true
+      t.string :email, :limit=>20 , :null => true, :unique => true
+      t.integer :hp, :default => 100
+      t.integer :lvl, :default => 1
+      t.integer :experience , :default => 0
+      t.integer :gold, :default => 0
+      t.integer :attack, :default => 10
+      t.integer :defence, :default => 5
+      t.integer :strength,  :default => 10
+      t.integer :agility,  :default => 10
       t.timestamps
     end
+    add_index("users","nick")
+  end
+
+  def down
+    drop_table :users
   end
 end
