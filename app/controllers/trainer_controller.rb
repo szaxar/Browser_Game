b3ff1c3579782f666ce00327753995f5c02de8ac
szaxar <wiktorleczynski@gmail.com>
2018-06-11 19:26:52 +0200

    adding incrementing method

--- app/controllers/trainer_controller.rb
+++ app/controllers/trainer_controller.rb
@@ -12,12 +12,54 @@ class TrainerController < ApplicationController
         user.gold = user.gold - upgrade_cost
         user.save
         flash[:success] = "Succes!"
-
-
     else
       flash[:warning] = "Not enough gold to upgrade strength!"
      end
-
     redirect_to(:action => :training)
   end
+
+  def incrementAttack
+    user = User.find(session[:user_id])
+    upgrade_cost = ApplicationHelper::ATTACK_COST* user.attack
+    if user.gold >= upgrade_cost
+      user.attack = user.attack+1
+      user.gold = user.gold - upgrade_cost
+      user.save
+      flash[:success] = "Succes!"
+    else
+      flash[:warning] = "Not enough gold to upgrade attack!"
+    end
+    redirect_to(:action => :training)
+  end
+
+
+  def incrementDefence
+    user = User.find(session[:user_id])
+    upgrade_cost = ApplicationHelper::DEFENCE_COST* user.defence
+    if user.gold >= upgrade_cost
+      user.defence = user.defence+1
+      user.gold = user.gold - upgrade_cost
+      user.save
+      flash[:success] = "Succes!"
+    else
+      flash[:warning] = "Not enough gold to upgrade defence!"
+    end
+    redirect_to(:action => :training)
+  end
+
+  def incrementAgility
+    user = User.find(session[:user_id])
+    upgrade_cost = ApplicationHelper::AGILITY_COST* user.agility
+    if user.gold >= upgrade_cost
+      user.agility = user.agility+1
+      user.gold = user.gold - upgrade_cost
+      user.save
+      flash[:success] = "Succes!"
+    else
+      flash[:warning] = "Not enough gold to upgrade agility!"
+    end
+    redirect_to(:action => :training)
+  end
+
+
 end
